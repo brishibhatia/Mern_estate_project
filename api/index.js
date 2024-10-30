39abae5c101c5393f0b5bb3526d9c08bbbfac472
YRishi Bhatia <brishi11128@gmail.com>
2024-10-30 16:37:40 +0530

    created a middleware and an error handler function

--- api/index.js
+++ api/index.js
@@ -21,3 +21,14 @@ app.listen(3000, () => {
 //  created a test api I was commiting a mistake by not checking at the localhost 3000 server
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+// middle ware created below
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
