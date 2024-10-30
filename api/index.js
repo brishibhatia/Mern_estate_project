ac6821c6a1d1ceadbfea5cc083f86c62cc0fe849
YRishi Bhatia <brishi11128@gmail.com>
2024-10-30 14:14:32 +0530

    created a test route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -14,3 +15,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is listening at port 3000!");
 });
+//  created a test api I was commiting a mistake by not checking at the localhost 3000 server
+app.use("/api/user", userRouter);
