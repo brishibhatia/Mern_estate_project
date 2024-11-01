5e1285622cd442a8b459f1c0a05938e2fa4e4236
YRishi Bhatia <brishi11128@gmail.com>
2024-11-01 11:27:08 +0530

    completed the signup page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/sign-in" element={<SignIn />}></Route>
-        <Route path="/sign-up" element={<SignUp />}></Route>
+        <Route path="/signIn" element={<SignIn />}></Route>
+        <Route path="/signUp" element={<SignUp />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
       </Routes>
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,38 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-export default function SignOut() {
-  return <div>SignOut</div>;
+export default function SignUp() {
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          placeholder="username"
+          className="p-3 border rounded-lg"
+          id="username"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          className="p-3 border rounded-lg"
+          id="username"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="p-3 border rounded-lg"
+          id="username"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-85">
+          sign Up
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Have an Account</p>
+        <Link to={"/signIn"}>
+          <span className="text-blue-800">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
 }
