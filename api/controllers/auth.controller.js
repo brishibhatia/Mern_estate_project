160d51b5c0442f198744d9b50663a146adb00e0c
YRishi Bhatia <brishi11128@gmail.com>
2024-11-01 14:51:31 +0530

    created sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.models.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   //   console.log(req.body); here we are saving the info in the database
@@ -14,5 +15,24 @@ export const signup = async (req, res, next) => {
     // res.status(500).json(error.message);
     next(error);
     // next(errorHandler(550, "Error from the function")); using errohandler function
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "user not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials "));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    // we are doing the above line so that we get info without the password
+    res
+      .cookie("access_token", token, { httponly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
