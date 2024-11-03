40b7b550f01582ea99a7b3e876e05221cb49c7e0
YRishi Bhatia <brishi11128@gmail.com>
2024-11-03 14:35:57 +0530

    added redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from "react";
+// import React from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// import { store } from "./redux/store.js";
+// import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom/client"; // if using React 18+
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </StrictMode>,
-)
+  </Provider>
+);
